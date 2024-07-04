4bed50aa9fc366d7f8804f132e69afc34498f9b3
EkateriniTri <136068628+EkateriniTri@users.noreply.github.com>
2024-07-04 18:52:01 +0200

    injected more weather data

--- index.js
+++ index.js
@@ -2,10 +2,45 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let feelElement = document.querySelector("#feel");
+  let feel = response.data.temperature.feels_like;
+  let windElement = document.querySelector("#wind");
+  let wind = response.data.wind.speed;
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
   cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
   temperatureElement.innerHTML = response.data.temperature.current;
   temperatureElement.innerHTML = Math.round(temperature);
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  feelElement.innerHTML = response.data.temperature.feels_like;
+  feelElement.innerHTML = Math.round(feel);
+  windElement.innerHTML = response.data.wind.speed;
+  windElement.innerHTML = Math.round(wind * 3.6);
+  timeElement.innerHTML = formatDate(date);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
